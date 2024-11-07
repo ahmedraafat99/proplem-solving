0cb5e4da28e7c8b2430cba2dcae56c25d3ae8c8f
ahmedraafat99 <ahmedraafat4954@gmail.com>
2024-11-07 17:18:22 +0200

    get character

--- prob2.py
+++ prob2.py
@@ -5,6 +5,6 @@ def get_ascii_char(number):
         return "Invalid input. Please provide a number between 0 and 127."
 
 # Examples
-print(get_ascii_char(65))  # Output: 'A'
-print(get_ascii_char(97))  # Output: 'a'
-print(get_ascii_char(48))  # Output: '0'
+print(get_ascii_char(65)) 
+print(get_ascii_char(97)) 
+print(get_ascii_char(48))  
