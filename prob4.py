49a1e6e45f57eac0049ec3f82eae86b1c90584c8
ahmedraafat99 <ahmedraafat4954@gmail.com>
2024-11-07 17:11:20 +0200

    p4

---
+++ prob4.py
@@ -0,0 +1,13 @@
+import math
+
+def distance(a, b):
+    return math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
+#example
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+a = Point(1, 2)
+b = Point(4, 6)
+print(distance(a, b))  
