8d96bb4a5fe31c3bfcc66a7724520e82ed2dc52d
ahmedraafat99 <ahmedraafat4954@gmail.com>
2024-11-07 17:13:15 +0200

    Update prob5.py

--- prob5.py
+++ prob5.py
@@ -4,7 +4,7 @@ def sum_array(arr):
         return 0
    
     return sum(arr) - max(arr) - min(arr)
-
+#EX
 print(sum_array([6, 2, 1, 8, 10]))  
 print(sum_array([1, 1, 11, 2, 3]))  
 print(sum_array([1]))               
