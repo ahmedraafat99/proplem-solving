f5f8a8ca9e09e8443d264b03abc309a75dff9dfe
ahmedraafat99 <ahmedraafat4954@gmail.com>
2024-11-07 17:17:09 +0200

    get chraracter

--- prob2.py
+++ prob2.py
@@ -1,2 +1,10 @@
-def ascii_char(value):
-    return chr(value)
+def get_ascii_char(number):
+    try:
+        return chr(number)
+    except ValueError:
+        return "Invalid input. Please provide a number between 0 and 127."
+
+# Examples
+print(get_ascii_char(65))  # Output: 'A'
+print(get_ascii_char(97))  # Output: 'a'
+print(get_ascii_char(48))  # Output: '0'
