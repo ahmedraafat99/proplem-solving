5321fbf33c303725dc5bd32ce5627e96ea9e5879
ahmedraafat99 <ahmedraafat4954@gmail.com>
2024-11-07 17:19:50 +0200

    is vowel here??

--- prob3.py
+++ prob3.py
@@ -1,7 +1,7 @@
 def replace_with_vowels(arr):
     # ASCII values for lowercase vowels
     vowel_codes = {97: 'a', 101: 'e', 105: 'i', 111: 'o', 117: 'u'}
-    
+    ##
     # Replace numbers with corresponding vowels if they match the ASCII values
     return [vowel_codes[num] if num in vowel_codes else num for num in arr]
 print(replace_with_vowels([97, 98, 99, 101, 120, 105]))  
